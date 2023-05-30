3e5d606021a6e110b2b2e711149cbb17c1fcbb31
Caíque Ramos <caique.ramos@dbmax.com.br>
2023-05-30 19:29:10 -0300

    implementacao novo front

---
+++ src/app/cadastrar/cadastrar.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Usuario } from '../model/Usuario';
+import { AuthService } from '../service/auth.service';
+
+@Component({
+  selector: 'app-cadastrar',
+  templateUrl: './cadastrar.component.html',
+  styleUrls: ['./cadastrar.component.css'],
+})
+export class CadastrarComponent implements OnInit {
+  usuario: Usuario = new Usuario();
+
+  tipo_conta: string;
+
+  constructor(
+    private auth: AuthService,
+    private router: Router
+    ) {}
+    
+    ngOnInit() {
+      window.scroll(0, 0);
+    }
+   
+    tipoUser(event: any) {
+      this.tipo_conta = event.target.value;
+    }
+  
+
+  cadastrar(){
+    this.usuario.tipo_conta = this.tipo_conta;
+
+    if(this.usuario.tipo_conta == this.tipo_conta) {
+      this.auth.cadastrar(this.usuario).subscribe((resp: Usuario) => {
+        this.usuario = resp;
+        alert('Empréstimo Cancelado com Sucesso')
+        this.router.navigate(['/entrar'])
+      })
+    } else {
+      alert('Preencha os dados corretos.')
+    }
+  }
+}
---
+++ src/app/model/UsuarioLogin.ts
@@ -0,0 +1,13 @@
+export class UsuarioLogin {
+  public id: number;
+  public nome: string;
+  public agencia: String;
+  public conta: String;
+  public usuario: string;
+  public senha: string;
+  public token: string;
+  public senha_digitos: string;
+  public cpf: string;
+  public telefone: String;
+  public tipo_conta: String;
+}
